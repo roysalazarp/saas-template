3f722464fb4f25ff636df94322c02f6e791c03d8
Roy Salazar <roy@livelaboratorio.fi>
2023-07-14 18:55:51 +0100

    added dummy api request to test SSR

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 import styles from "@/app/page.module.css";
 
-export default function Home() {
+async function getData() {
+  const res = await fetch("https://dummyjson.com/quotes/1");
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
   return (
     <main className={styles.main}>
-      <h1 className={styles.page_title}>Hello world</h1>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <h1 className={styles.page_title}>Hello world</h1>
+        <p>{data.quote}</p>
+      </div>
     </main>
   );
 }
